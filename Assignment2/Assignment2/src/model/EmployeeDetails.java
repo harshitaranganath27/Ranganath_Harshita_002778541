51d0065bbee333d9809b62f86dfc3b6765c50f02
harshitaranganath27 <ranganath.h@northeastern.edu>
2022-10-27 16:21:03 -0400

    classed added

---
+++ Assignment2/Assignment2/src/model/EmployeeDetails.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sirius
+ */
+public class EmployeeDetails {
+    private ArrayList<Employee> empDetailsList;
+    
+    public EmployeeDetails(){
+        this.empDetailsList = new ArrayList<Employee>();
+    }
+
+    public ArrayList<Employee> getEmpDetailsList() {
+        return empDetailsList;
+    }
+
+    public void setEmpDetailsList(ArrayList<Employee> empDetailsList) {
+        this.empDetailsList = empDetailsList;
+    }
+    
+    public Employee addNewEmployeeDetails(){
+        Employee employee = new Employee();
+        empDetailsList.add(employee);
+        return employee;
+    }
+
+    public void deleteEmplDetails(Employee selectedEmployee) {
+        empDetailsList.remove(selectedEmployee);
+        }
+}
---
+++ JavaApplication1/src/model/Employee.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author sushmithaprathap
+ */
+public class Employee {
+
+    private String name;
+    private String eId;
+    private String age;
+    private String gender;
+    private String startDate;
+    private String level;
+    private String teamInfo;
+    private String positionTitle;
+    private String photo;
+    private String phoneNumber;
+    private String emailId;
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String geteId() {
+        return eId;
+    }
+
+    public void seteId(String eId) {
+        this.eId = eId;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    @Override  //tells java that we'll be overriding somthing that inherits
+    public String toString() {
+        return eId;
+    }
+}
